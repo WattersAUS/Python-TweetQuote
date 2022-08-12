535421fcf61fb7d2914b731231737942ad48e9c5
Graham Watson <grahamj.watson@gmail.com>
2022-08-12 17:39:26 +0100

    Expanded Error Handling
    1. Add GeneralError class to enable bespoke error messages.
    2. Enabled unpacking of JSON API error or HTML error and delivery to new class for display
    3. Added random file selection from images directory (not using image yet!)

--- tweetquote.py
+++ tweetquote.py
@@ -10,6 +10,8 @@ from telnetlib import theNULL
 import requests
 import tweepy
 import yaml
+import glob
+import random
 
 #
 # sample response from service
@@ -37,15 +39,28 @@ import yaml
 #        }
 #    }
 
-script_version = 1.01
+script_version = 1.03
+
+class GeneralError(Exception):
+    """Exception raised for catching various problems.
+
+    Attributes:
+        code -- this may be either a default or a code from the service
+        message -- we might get a message to use
+    """
+    def __init__(self, code, message="Unknown Error"):
+        self.code = code
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.code} -> {self.message}'
 
 # get current dt and print
-
 def getCurrentDateTimeAsString():
     return datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
 
 # general print for logging
-
 def printProgress(stage, extra):
     message = '[ ' + getCurrentDateTimeAsString() + ' ]:' + os.path.basename(__file__) + ':' + 'v{:.2f}'.format(script_version) + ':' + stage
     if extra != '':
@@ -53,45 +68,54 @@ def printProgress(stage, extra):
     print(message)
     return
 
-# should the api call crash and burn, die gracefully
+# get list of image files we could use and return a random item
+def getRandomFile(path):
+    fileList = glob.glob(path)
+    if len(fileList) == 0:
+        raise GeneralError(0, "Failed to find images to use!")
+    return random.choice(fileList)
 
-class APIResponseError(Exception):
-    pass
+# do we have a JSON str
+def isJSON(str):
+    try:
+        json.loads(str)
+    except ValueError as e:
+        return False
+    return True
 
-def api_error_response(response):
-    printProgress('API error', json.dumps(response.json()))
-    raise APIResponseError(response)
-
-# get a random quote but we need to be mindful we don't exceed the max length of a tweet
-
+# get a random quote
 def getRandomQuote(apiUrl, apiToken):
-    headersAPI = {
+    headers_api = {
         'accept': 'application/json',
         'Authorization': 'Bearer ' + apiToken,
     }
     params = (
     )
-    response = requests.get(apiUrl, headers=headersAPI, params=params, verify=False)
-    if response.status_code == 200:
-        return response.json()
-
-    api_error_response(response)
-    return api_response
+    response = requests.get(apiUrl, headers=headers_api, params=params, verify=False)
+    # on an error check if the API returned an internal api generated error (in JSON) or a tragic HTML response error ie. 404
+    if response.status_code != 200:
+        if isJSON(response.content.decode()):
+            json_string = response.json()
+            raise GeneralError(json_string['code'], json_string['message'])
+        else:
+            raise GeneralError(response.status_code, "API crashed and burned!")
+    return response.json()
 
 def main():
-
     try:
         printProgress('Starting', '')
         with open('tweetquote.yaml', 'r') as ymlfile:
             cfg = yaml.full_load(ymlfile)
 
-# get the quote 
+        # from the repo of images get a random one (we may not use this yet)
+        image_file = getRandomFile(cfg['images']['path'])
+        printProgress('Image file', image_file)
 
+        # get the quote 
         json_string = getRandomQuote(cfg['api']['url'], cfg['api']['token'])
         tweet =  '\'' + json_string['author']['quote']['text'] + '\'\n\n' + json_string['author']['name']
 
-# Using authentication keys created on the Twitter dev account to make API calls
-
+        # go and tweet!
         auth = tweepy.OAuthHandler(
                 cfg['twitter_auth_keys']['consumer_key'],
                 cfg['twitter_auth_keys']['consumer_secret']
@@ -102,9 +126,10 @@ def main():
                 )
 
         printProgress('Tweeting', json.dumps(json_string))
-        api = tweepy.API(auth)
-
-        status = api.update_status(status=tweet)
+#        api = tweepy.API(auth)
+#        status = api.update_status(status=tweet)
+    except GeneralError:
+        print()
     finally:
         printProgress('Finishing', '')
 
